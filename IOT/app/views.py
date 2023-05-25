230b7d41aa74a01fb8667f3f7e4d1c073c39535a
GekBootje <ewoud09@gmail.com>
2023-05-25 23:00:56 +0200

    Graph1

---
+++ IOT/app/views.py
@@ -0,0 +1,20 @@
+from app import app
+from flask import render_template
+
+
+@app.route("/")
+def homepage():
+    return render_template("public/login.html")
+
+
+@app.route("/home")
+def test1():
+    return render_template("public/home.html")
+
+@app.route("/competitie")
+def test():
+    return render_template("public/competitie.html")
+
+@app.route("/graphwb")
+def graph():
+    return render_template("public/graphwb.html")
