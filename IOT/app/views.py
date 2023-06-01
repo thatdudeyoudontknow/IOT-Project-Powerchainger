9f7083a5004145b19d2f0793301f520ef3773fbd
GekBootje <ewoud09@gmail.com>
2023-06-01 16:00:20 +0200

    Login Start 1
    login/logout routing in views en login.html geupdate

--- IOT/app/views.py
+++ IOT/app/views.py
@@ -1,9 +1,6 @@
 from app import app
 from flask import render_template
-
-@app.route("/login")
-def login():
-    return render_template("public/login.html")
+from flask_login import LoginManager
 
 
 @app.route("/")
@@ -21,3 +18,24 @@ def bezuinigen():
 @app.route("/graph")
 def graph():
     return render_template("public/graph.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        if not url_has_allowed_host_and_scheme(next, request.host):
+            return flask.abort(400)
+        return flask.redirect(next or flask.url_for('index'))
+    return render_template("public/login.html", form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/home")
