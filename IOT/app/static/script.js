38a645ea98e85c5c60b01ba374c7c5bc7f4ee3e2
Leendert-code <74918403+Leendert-code@users.noreply.github.com>
2023-06-16 15:23:56 +0200

    Views werkt weer zonder errors

--- IOT/app/static/script.js
+++ IOT/app/static/script.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   let currentChart = null; // Variable to keep track of the current chart
 
   function fetchVerbruik() {
     axios.get('/huidig_verbruik')
       .then(response => {
         const verbruikElement = document.getElementById('huidig_verbruik');
-  
+
         if (response.data.error) {
           verbruikElement.textContent = response.data.error;
         } else {
@@ -17,12 +17,12 @@ document.addEventListener('DOMContentLoaded', function() {
         console.error('Error:', error);
       });
   }
-  
+
   function fetchHuidigeWoning() {
     axios.get('/huidige_woning')
       .then(response => {
         const woningElement = document.getElementById('huidige_woning');
-  
+
         if (response.data.error) {
           woningElement.textContent = response.data.error;
         } else {
@@ -34,16 +34,6 @@ document.addEventListener('DOMContentLoaded', function() {
         console.error('Error:', error);
       });
   }
-  
-  function fetchCurrentValues() {
-    fetchVerbruik();
-    fetchHuidigeWoning();
-  }
-  
-
-  // Fetch the current values when the page is loaded
-
-
 
   function fetchTotalValue() {
     axios.get('/verbruik_per_dag')
@@ -52,10 +42,11 @@ document.addEventListener('DOMContentLoaded', function() {
   
         if (response.data.error) {
           valueElement.textContent = response.data.error;
-        } else {
-          const values = response.data.values;
-          const total = values.reduce((acc, entry) => acc + entry.value, 0); // Access the 'value' property of each entry
+        } else if (response.data.value !== undefined) {
+          const total = response.data.value;
           valueElement.textContent = `${total} KW`;
+        } else {
+          valueElement.textContent = 'No data available';
         }
       })
       .catch(error => {
@@ -63,20 +54,23 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   }
   
-  function fetchCurrentValues() {
-    fetchVerbruik();
+
+  if (document.getElementById('huidige_woning')) {
     fetchHuidigeWoning();
+  }
+
+  if (document.getElementById('verbruik_per_dag')) {
     fetchTotalValue();
   }
 
-  fetchCurrentValues();
+  if (document.getElementById('huidig_verbruik')) {
+    fetchVerbruik();
+  }
 
-  
 
-  fetch("/data")
+  fetch('/data')
     .then(response => response.json())
     .then(data => {
-
       // maakt of update de grafiek
       function createOrUpdateChart(labels, values) {
         const ctx = document.getElementById('Grafieken').getContext('2d');
@@ -87,41 +81,41 @@ document.addEventListener('DOMContentLoaded', function() {
           currentChart.data.labels = labels;
           currentChart.data.datasets[0].data = values;
           currentChart.update();
-
         } else {
           // maak een nieuwe grafiek
           currentChart = new Chart(ctx, {
             type: 'line',
             data: {
               labels: labels,
-              datasets: [{
-                label: 'Messages',
-                data: values,
-                fill: true,
-                borderColor: 'blue',
-                borderWidth: 1,
-                pointBorderWidth: 0,
-
-              }]
+              datasets: [
+                {
+                  label: 'verbruiks',
+                  data: values,
+                  fill: true,
+                  borderColor: 'blue',
+                  borderWidth: 1,
+                  pointBorderWidth: 0,
+                },
+              ],
             },
             // OPTIES AANPASSEN OM DE GRAFIEK AAN TE PASSEN
-             options: { 
-                scales: {
-                  x: {
-                    grid: {
-                      display: false // Hide the x-axis grid lines
-                    }
+            options: {
+              scales: {
+                x: {
+                  grid: {
+                    display: false, // Hide the x-axis grid lines
                   },
-                  y: {
-                    beginAtZero: true // Start the y-axis from zero
-                  }
                 },
-                plugins: {
-                  legend: {
-                    display: false // Hide the legend
-                  }
-              }
-            }
+                y: {
+                  beginAtZero: true, // Start the y-axis from zero
+                },
+              },
+              plugins: {
+                legend: {
+                  display: false, // Hide the legend
+                },
+              },
+            },
           });
         }
       }
@@ -132,95 +126,97 @@ document.addEventListener('DOMContentLoaded', function() {
       const btnWeek = document.getElementById('btnWeek');
       const btnMonth = document.getElementById('btnMonth');
 
-      btnHour.addEventListener('click', function() {
-
-        // pak de data en zorg dat alleen het UUR van de huidige dag word gepakt
+      function filterDataByHour(data) {
         const currentDate = new Date().toLocaleDateString();
         const currentHour = new Date().getHours();
 
-        const filteredData = data.filter(item => {
-          const itemDate = new Date(item.timestamp).toLocaleDateString();
-          const itemHour = new Date(item.timestamp).getHours();
+        return data.filter(item => {
+          const itemDate = new Date(item.datetime).toLocaleDateString();
+          const itemHour = new Date(item.datetime).getHours();
           return itemDate === currentDate && itemHour === currentHour;
         });
+      }
 
-        const labels = filteredData.map(item => new Date(item.timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
-        const values = filteredData.map(item => item.message);
+      function filterDataByDay(data) {
+        const currentDate = new Date().toLocaleDateString();
+
+        return data.filter(item => {
+          const itemDate = new Date(item.datetime).toLocaleDateString();
+          return itemDate === currentDate;
+        });
+      }
+
+      function filterDataByWeek(data) {
+        const currentWeek = moment().week();
+
+        return data.filter(item => {
+          const itemWeek = moment(item.datetime).week();
+          return itemWeek === currentWeek;
+        });
+      }
+
+      function filterDataByMonth(data) {
+        const currentMonth = new Date().getMonth();
+
+        return data.filter(item => {
+          const itemMonth = new Date(item.datetime).getMonth();
+          return itemMonth === currentMonth;
+        });
+      }
+
+      function updateChartWithFilteredData(filteredData) {
+        const labels = filteredData.map(item =>
+          new Date(item.datetime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+        );
+        const values = filteredData.map(item => item.verbruik);
 
         createOrUpdateChart(labels, values);
+      }
 
-        // zorg dat deze knop een geselecteerd vinkje krijgt en de abdere knoppen niet 
+      btnHour.addEventListener('click', function () {
+        const filteredData = filterDataByHour(data);
+        updateChartWithFilteredData(filteredData);
+
+        // zorg dat deze knop een geselecteerd vinkje krijgt en de abdere knoppen niet
         btnHour.classList.add('selected');
         btnDay.classList.remove('selected');
         btnWeek.classList.remove('selected');
         btnMonth.classList.remove('selected');
       });
 
-      btnDay.addEventListener('click', function() {
-        // pak de data en zorg dat alleen de DAG word gepakt
-        const currentDate = new Date().getDate();
+      btnDay.addEventListener('click', function () {
+        const filteredData = filterDataByDay(data);
+        updateChartWithFilteredData(filteredData);
 
-        const filteredDataDay = data.filter(item => {
-          const itemDate = new Date(item.timestamp).getDate();
-          return itemDate === currentDate;
-        });
-
-        const labels = filteredDataDay.map(item => new Date(item.timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
-        const values = filteredDataDay.map(item => item.message);
-
-        createOrUpdateChart(labels, values);
-
-        // zorg dat deze knop een geselecteerd vinkje krijgt en de abdere knoppen niet 
+        // zorg dat deze knop een geselecteerd vinkje krijgt en de abdere knoppen niet
         btnHour.classList.remove('selected');
         btnDay.classList.add('selected');
         btnWeek.classList.remove('selected');
         btnMonth.classList.remove('selected');
-
-        // Update the other buttons (btn3, btn4, btn5) as needed
       });
 
-      btnWeek.addEventListener('click', function() {
-        // pak de data en zorg dat alleen de WEEK word gepakt
-        const currentWeek = moment().week();
+      btnWeek.addEventListener('click', function () {
+        const filteredData = filterDataByWeek(data);
+        updateChartWithFilteredData(filteredData);
 
-        const filteredDataWeek = data.filter(item => {
-          const itemWeek = moment(item.timestamp).week();
-          return itemWeek === currentWeek;
-        });
-
-        const labels = filteredDataWeek.map(item => moment(item.timestamp).format('ll'));
-        const values = filteredDataWeek.map(item => item.message);
-
-        createOrUpdateChart(labels, values);
-
-
-        // zorg dat deze knop een geselecteerd vinkje krijgt en de abdere knoppen niet 
+        // zorg dat deze knop een geselecteerd vinkje krijgt en de abdere knoppen niet
         btnHour.classList.remove('selected');
         btnDay.classList.remove('selected');
         btnWeek.classList.add('selected');
         btnMonth.classList.remove('selected');
       });
 
-      btnMonth.addEventListener('click', function() {
-        // pak de data en zorg dat alleen de MAAND word gepakt
-        const currentMonth = new Date().getMonth();
+      btnMonth.addEventListener('click', function () {
+        const filteredData = filterDataByMonth(data);
+        updateChartWithFilteredData(filteredData);
 
-        const filteredDataMonth = data.filter(item => {
-          const itemMonth = new Date(item.timestamp).getMonth();
-          return itemMonth === currentMonth;
-        });
-
-        const labels = filteredDataMonth.map(item => moment(item.timestamp).format('ll'));
-        const values = filteredDataMonth.map(item => item.message);
-
-        createOrUpdateChart(labels, values);
-
-        // zorg dat deze knop een geselecteerd vinkje krijgt en de abdere knoppen niet 
+        // zorg dat deze knop een geselecteerd vinkje krijgt en de abdere knoppen niet
         btnHour.classList.remove('selected');
         btnDay.classList.remove('selected');
         btnWeek.classList.remove('selected');
         btnMonth.classList.add('selected');
       });
+
       btnHour.click();
     })
     .catch(error => {
--- IOT/app/views.py
+++ IOT/app/views.py
@@ -31,30 +31,30 @@ def login2():
     return render_template("public/login2.html")
 
 @app.route("/huidige_woning")
-def get_huidige_woning():
-    userID = 2
-    # Connect to the SQLite database
-    database_path = os.path.join(os.path.dirname(__file__), 'data.sqlite')
+def get_current_huisnaam():
+    userID = 1  # Replace with the actual current user ID
 
     # Connect to the SQLite database
+    database_path = os.path.join(os.path.dirname(__file__), 'data.sqlite')
     conn = sqlite3.connect(database_path)
     cursor = conn.cursor()
 
-    # Execute a query to retrieve data from the database
+    # Execute a query to retrieve the current huisnaam from the current user
     query = "SELECT huisnaam FROM huis WHERE huisID IN (SELECT huisID FROM HKU WHERE userID = ?)"
-    cursor.execute(query, (userID,))  # Pass the userID as a parameter
-    
-    # Fetch all the results
-    results = cursor.fetchall()
+    cursor.execute(query, (userID,))
+
+    # Fetch the result
+    result = cursor.fetchone()
 
     # Close the cursor and database connection
     cursor.close()
     conn.close()
 
-    # Process the results as needed
-    # ...
-
-    return "Data retrieved successfully"
+    if result is not None:
+        huisnaam = result[0]
+        return jsonify({'value': huisnaam})  # Return JSON response
+    else:
+        return jsonify({'error': 'No current huisnaam found for the user'})  # Return JSON response
 
 
 
@@ -85,7 +85,7 @@ def get_data():
     for row in rows:
         timestamp = row[0]
         message = row[1]
-        results.append({'timestamp': timestamp, 'message': message})
+        results.append({'datetime': timestamp, 'verbruik': message})
 
     return jsonify(results)
 
@@ -93,6 +93,7 @@ def get_data():
 # data word uit de database gehaald om de laatste waardes op te halen voor het huidig verbruik
 @app.route('/huidig_verbruik')
 def get_current():
+    userID = 1
     # Get the absolute path of the database file in the current directory
     database_path = os.path.join(os.path.dirname(__file__), 'data.sqlite')
 
@@ -101,8 +102,8 @@ def get_current():
     cursor = conn.cursor()
 
     # Execute a query to retrieve the latest numerical value from the database
-    query = "SELECT message FROM mqtt_messages ORDER BY timestamp DESC LIMIT 1"
-    cursor.execute(query)
+    query = "SELECT verbruik FROM verbruik WHERE huisID IN (SELECT huisID FROM HKU WHERE userID = ?) ORDER BY datetime DESC LIMIT 1"
+    cursor.execute(query, (userID,))  # Pass the userID as a parameter
 
     # Fetch the latest message
     row = cursor.fetchone()
@@ -119,6 +120,34 @@ def get_current():
 
     return jsonify({'value': numerical_value})
 
+@app.route('/verbruik_per_dag')
+def get_total():
+    userID = 1
+    # Get the absolute path of the database file in the current directory
+    database_path = os.path.join(os.path.dirname(__file__), 'data.sqlite')
+
+    # Connect to the SQLite database
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+
+    # Execute a query to retrieve the sum of verbruik values for the same day
+    query = "SELECT SUM(verbruik) FROM verbruik WHERE huisID IN (SELECT huisID FROM HKU WHERE userID = ?) AND DATE(datetime) = DATE('now', 'localtime')"
+    cursor.execute(query, (userID,))  # Pass the userID as a parameter
+
+    # Fetch the sum of verbruik values
+    row = cursor.fetchone()
+
+    # Close the database connection
+    conn.close()
+
+    if row[0] is None:
+        # No verbruik values found for the current day
+        return jsonify({'error': 'No verbruik values found for the current day.'})
+
+    # Extract the sum of verbruik values
+    total_verbruik = float(row[0])
+
+    return jsonify({'value': total_verbruik})  # Wrap the total_verbruik value in a dictionary
 
 if __name__ == '__main__':
     app.run()
